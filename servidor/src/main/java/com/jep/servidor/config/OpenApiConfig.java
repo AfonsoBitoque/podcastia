f847090027b1d9e6d2d15771fd9a15d9e62e3c8b
a79146 <a79146@ualg.pt>
2026-03-24 22:13:32 +0000

    POD-84: Finalizacaoo da implementacao do SpringDoc

--- servidor/src/main/java/com/jep/servidor/config/OpenApiConfig.java
+++ servidor/src/main/java/com/jep/servidor/config/OpenApiConfig.java
@@ -2,6 +2,7 @@ package com.jep.servidor.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,11 +10,12 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfig {
 
   @Bean
-  public OpenAPI customOpenApi() {
+  public OpenAPI customOpenAPI() {
     return new OpenAPI()
         .info(new Info()
-            .title("API Podcastia")
+            .title("Podcastia API")
             .version("v1.0.0")
-            .description("Documentação da API REST do projeto Podcastia."));
+            .description("Documentação da API REST do projeto Podcastia.")
+            .license(new License().name("Apache 2.0").url("http://springdoc.org")));
   }
 }
